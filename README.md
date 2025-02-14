71226d3831a258902e812546fd52c0a9ac4c6741
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-02-14 18:08:03 +0000

    nav bar

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # responsive-car-website-design
+
+
+- Responsive Car Website Design Using HTML CSS And JavaScript          (p)
+- Contains sliding images with the Swiper js library.          (p)
+- Contains animated images with the Gsap Js library.          (p)
+- Developed first with the Mobile First methodology, then for desktop.          (p)
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.          (p)
---
+++ main.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navMenu = document.getElementById("nav-menu"),
+          navToggle = document.getElementById("nav-toggle"),
+          navClose = document.getElementById("nav-close");
+
+    if (navToggle) {
+        navToggle.addEventListener("click", () => {
+            navMenu.classList.add("show-menu");
+        });
+    }
+
+    if (navClose) {
+        navClose.addEventListener("click", () => {
+            navMenu.classList.remove("show-menu");
+        });
+    }
+});
+
+// remove menu
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
