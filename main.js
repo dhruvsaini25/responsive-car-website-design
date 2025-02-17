b90100e7f8362d5c4580460aa77e301515b75db1
dhruvsaini25 <147823527+dhruvsaini25@users.noreply.github.com>
2025-02-17 22:23:20 +0000

    final

--- main.js
+++ main.js
@@ -50,6 +50,13 @@ const swiperHome = new Swiper('.home__swiper', {
     // },
 });
 
+//gsap animation
+gsap.from('.home__panel-1', {y: -1000, duration: 2})
+gsap.from('.home__panel-2', {y: 1000, duration: 2})
+gsap.from('.home__image', {x: 1000, duration: 2})
+gsap.from('.home__titles', {y: 100, opacity:0, delay: 2})
+gsap.from('.home__title', {y: 100, opacity: 0, duration: 2.1})
+
 //blur header
 const blurHeader = () =>{
     const header = document.getElementById('header')
